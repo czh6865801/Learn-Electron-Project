e35007972b84245ba4f7eb5063f2b26632ca8f8a
czh6865801 <2251668603@qq.com>
2020-08-27 23:01:00 +0800

    添加新项目

---
+++ study_electron_2/src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'study_electron_2'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
